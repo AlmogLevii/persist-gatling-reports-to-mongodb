50de0dc3c257e4f9c3ae3569bc85962de32a6856
jude <jude.niroshan11@gmail.com>
2021-08-17 23:48:17 +0200

    refactor code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
+import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
+import java.util.stream.Stream;
 
 //TODO:
 //1. find a way to get the pathToTarget dynamically
@@ -14,16 +15,16 @@ import java.util.Scanner;
 
 public class Main {
 
-    final static String pathToTarget = "//home/allevi/kogitoRepos/kogito-benchmark/test/target/gatling/";
+    public static final String GATLING_FILE_PATH_ENV = "GATLING_BASE_FILE_PATH";
+    public static final String gatlingBasePath = getGatlingBasePath();
 
     public static void main(String[] args) {
 
         JSONParser parser = new JSONParser();
-        try {
 
-            String jsonFileName = getJsonFileName(getSimulationID());
-            JSONObject jsonObject = (JSONObject) parser.parse(new FileReader(jsonFileName));
+        try(FileReader reader = new FileReader(getStatisticFilePath())) {
 
+            final JSONObject jsonObject = (JSONObject) parser.parse(reader);
 
             Statistic numberOfRequests = new Statistic(jsonObject.get("numberOfRequests"));
             Statistic minResponseTime = new Statistic(jsonObject.get("minResponseTime"));
@@ -39,30 +40,33 @@ public class Main {
             RequestsGroup failedRequests = new RequestsGroup(jsonObject.get("group4"));
             SuccessfulRequests successfulRequests = new SuccessfulRequests(jsonObject);
 
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static String getSimulationID() throws IOException {
-        String lastRunFileName =  pathToTarget + "lastRun.txt";
-        Path path = Paths.get(lastRunFileName);
-
-        Scanner scanner = new Scanner(path);
-
-        String line = scanner.nextLine();
-
-        scanner.close();
-
-        return line;
+    private static String getGatlingBasePath() {
+        String providedPath = System.getenv(GATLING_FILE_PATH_ENV);
+        if (providedPath == null) {
+            throw new RuntimeException(GATLING_FILE_PATH_ENV + " env is not available.");
+        } else if (providedPath.isEmpty()) {
+            throw new RuntimeException(GATLING_FILE_PATH_ENV + " env value is empty.");
+        } else if (providedPath.endsWith(File.separator)) {
+            providedPath = providedPath.substring(0, providedPath.lastIndexOf(File.separator));
+        }
+        return providedPath;
     }
 
-    private static String getJsonFileName(String simulationID) {
-        String prefix = pathToTarget;
-        String suffix = "/js/global_stats.json";
-        return prefix + simulationID + suffix;
+    private static String getStatisticFilePath() throws IOException {
+        String lastRunFilePath = gatlingBasePath + File.separator + "lastRun.txt";
+
+        try (Stream<String> fileContentLines = Files.lines(Paths.get(lastRunFilePath))) {
+            final String simulationID = fileContentLines.findFirst()
+                    .orElseThrow(() -> new RuntimeException("lastRun.txt file is empty!"));
+            final String suffix = "/js/global_stats.json";
+
+            return gatlingBasePath + File.separator + simulationID + suffix;
+        }
     }
 
 }
