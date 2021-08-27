dc70d557c7ea03a14e1e0337ad580951cc792976
jude <jude.niroshan11@gmail.com>
2021-08-27 19:28:47 +0200

    refactor code and improve documentation

--- src/main/java/com/redhat/kogito/benchmark/Main.java
+++ src/main/java/com/redhat/kogito/benchmark/Main.java
@@ -1,23 +1,17 @@
+package com.redhat.kogito.benchmark;
+
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.bson.Document;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
-
-//TODO:
-//1. find a way to get the pathToTarget dynamically
-//2. add the program to the yaml file that Lokesh created
-//3. understand the new task "Upload Gatling Metrics into DB"
 
 public class Main {
 
@@ -38,25 +32,9 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        try (FileReader reader = new FileReader(getStatisticFilePath())) {
+        try {
+            String readString = Files.readString(Paths.get(getStatisticFilePath()), StandardCharsets.UTF_8);
             LOGGER.info("path to the json file: " + getStatisticFilePath());
-            final JSONParser parser = new JSONParser();
-            final JSONObject jsonObject = (JSONObject) parser.parse(reader);
-
-/*            Statistic numberOfRequests = new Statistic(jsonObject.get("numberOfRequests"));
-            Statistic minResponseTime = new Statistic(jsonObject.get("minResponseTime"));
-            Statistic maxResponseTime = new Statistic(jsonObject.get("maxResponseTime"));
-            Statistic meanResponseTime = new Statistic(jsonObject.get("meanResponseTime"));
-            Statistic standardDeviation = new Statistic(jsonObject.get("standardDeviation"));
-            Statistic percentile50 = new Statistic(jsonObject.get("percentiles1"));
-            Statistic percentile575 = new Statistic(jsonObject.get("percentiles2"));
-            Statistic percentile95 = new Statistic(jsonObject.get("percentiles3"));
-            Statistic percentile99 = new Statistic(jsonObject.get("percentiles4"));
-            Statistic meanNumberOfRequestsPerSecond = new Statistic(jsonObject.get("meanNumberOfRequestsPerSecond"));
-
-            RequestsGroup failedRequests = new RequestsGroup(jsonObject.get("group4"));
-            SuccessfulRequests successfulRequests = new SuccessfulRequests(jsonObject);*/
-
 
             var mongoConnectionString = envVariableExtractor(MONGODB_CONNECTION_URI);
 
@@ -64,7 +42,7 @@ public class Main {
                 var db = mongoClient.getDatabase(envVariableExtractor(MONGODB_NAME));
                 var collection = db.getCollection(envVariableExtractor(MONGODB_COLLECTION_NAME));
 
-                final Document gatlingResult = new Document(LAST_SIMULATION_ID, jsonObject);
+                final Document gatlingResult = new Document(LAST_SIMULATION_ID, Document.parse(readString));
                 collection.insertOne(gatlingResult);
 
                 LOGGER.info("gatling results saved successfully in " +
@@ -72,8 +50,6 @@ public class Main {
                         + " database.");
             }
 
-        } catch (ParseException e) {
-            LOGGER.error("Gatling test results json file could not be parsed properly. ", e);
         } catch (Exception e) {
             LOGGER.error("Main method failed due to: ", e);
         }
